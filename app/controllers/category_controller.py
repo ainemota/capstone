7d4c210dcdc6ca942dd91041063e56565fa85d16
Victor Nunes <victornunes@pop-os.localdomain>
2022-05-03 13:26:22 -0300

    feat: post, get, patch and delete category

--- app/controllers/category_controller.py
+++ app/controllers/category_controller.py
@@ -1,21 +1,56 @@
+from http import HTTPStatus
+
+from flask_jwt_extended import jwt_required
+from sqlalchemy.orm import Session
+
 from app.configs.database import db
+from flask import request, jsonify
+
+from app.models.category_model import CategoryModel
 
 
 def post_category():
-    return ""
+    data = request.get_json()
+
+    category = CategoryModel(name=data["name"], description=data["description"])
+
+    category.create()
+
+    return jsonify(category), HTTPStatus.CREATED
 
 
 def get_category():
-    return ""
+    session: Session = db.session()
+
+    categories = session.query(CategoryModel).all()
+
+    return jsonify(categories), HTTPStatus.OK
 
 
 def patch_category(category_id):
-    return ""
+    data = request.get_json()
+    session: Session = db.session()
+
+    category = session.query(CategoryModel).get(category_id)
+
+    category.update(data)
+
+    return jsonify(category), HTTPStatus.OK
 
 
 def delete_category(category_id):
-    return ""
+    session: Session = db.session()
+
+    category = session.query(CategoryModel).get(category_id)
+
+    category.delete()
+
+    return "", HTTPStatus.NO_CONTENT
 
 
 def get_by_name_category(name):
-    return ""
+    session: Session = db.session()
+
+    category = session.query(CategoryModel).filter(CategoryModel.name == name).first()
+
+    return jsonify(category), HTTPStatus.OK
