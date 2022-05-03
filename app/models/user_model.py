95878aa45c9e9f757937b6d5065bc53215e039fa
ainemota <motaaine1@gmail.com>
2022-05-03 12:55:15 -0300

    feat: foreingKey from address added to UserModel

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,9 +1,12 @@
+from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.configs.database import db
 from dataclasses import dataclass
 from uuid import uuid4
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from app.models.address_model import Address
 
 
 @dataclass
@@ -11,13 +14,15 @@ class UserModel(db.Model):
     id: str
     name: str
     email: str
-
+    address: Address
     __tablename__ = "users"
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String(80), nullable=False, unique=True)
     password_hash = db.Column(db.String, nullable=True)
+    address_id = db.Column(UUID(as_uuid=True), ForeignKey("addresses.id"), nullable=False)
+    address = relationship("Address")
 
     @property
     def password(self):
