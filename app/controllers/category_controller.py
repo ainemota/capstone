eab07e3ecd591f44ff1a9e26d6317531ec9790dc
Victor Nunes <victornunes@pop-os.localdomain>
2022-05-03 11:16:37 -0300

    feat: added categories route

---
+++ app/controllers/category_controller.py
@@ -0,0 +1,21 @@
+from app.configs.database import db
+
+
+def post_category():
+    return ""
+
+
+def get_category():
+    return ""
+
+
+def patch_category(category_id):
+    return ""
+
+
+def delete_category(category_id):
+    return ""
+
+
+def get_by_name_category(name):
+    return ""
